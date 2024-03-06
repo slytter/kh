27c6ca18c19c25a6df049e96bcc9a95301ae103a
Schlüter <nikolaj.sn@hotmail.com>
2024-03-07 00:24:08 +0100

    Add basic auth

--- app/root.tsx
+++ app/root.tsx
@@ -1,5 +1,6 @@
 import { cssBundleHref } from "@remix-run/css-bundle";
-import type { LinksFunction } from "@remix-run/node";
+import type { LinksFunction, LoaderFunctionArgs } from "@remix-run/node";
+import { createSupabaseServerClient } from "./utils/supabase.server";
 import {
   Links,
   LiveReload,
@@ -7,23 +8,69 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
+  json,
+  useLoaderData,
+  useRevalidator,
 } from "@remix-run/react";
+import { useEffect, useState } from "react";
+import { createBrowserClient } from "@supabase/auth-helpers-remix";
+import stylesheet from "~/tailwind.css";
+
+export const loader = async ({ request }: LoaderFunctionArgs) => {
+  const env = {
+    SUPABASE_URL: process.env.SUPABASE_URL!,
+    SUPABASE_PUBLIC_KEY: process.env.SUPABASE_PUBLIC_KEY!,
+  };
+
+  const response = new Response();
+
+  const supabase = createSupabaseServerClient({ request, response });
+
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+
+  return json({ env, session }, { headers: response.headers });
+};
 
 export const links: LinksFunction = () => [
   ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
+  { rel: "stylesheet", href: stylesheet },
 ];
 
 export default function App() {
+  const { env, session } = useLoaderData<typeof loader>();
+  const { revalidate } = useRevalidator();
+
+  const [supabase] = useState(() =>
+    createBrowserClient(env.SUPABASE_URL, env.SUPABASE_PUBLIC_KEY),
+  );
+
+  const serverAccessToken = session?.access_token;
+
+  useEffect(() => {
+    const {
+      data: { subscription },
+    } = supabase.auth.onAuthStateChange((event, session) => {
+      if (session?.access_token !== serverAccessToken) {
+        revalidate();
+      }
+    });
+    return () => {
+      subscription.unsubscribe();
+    };
+  }, [supabase.auth, serverAccessToken, revalidate]);
+
   return (
     <html lang="en">
       <head>
         <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
         <Meta />
         <Links />
       </head>
       <body>
-        <Outlet />
+        <Outlet context={{ supabase, session }} />
         <ScrollRestoration />
         <Scripts />
         <LiveReload />
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,7 @@
 import type { MetaFunction } from "@remix-run/node";
+import { useOutletContext } from "@remix-run/react";
+import { Login } from "~/components/Login";
+import { OutletContext } from "~/types";
 
 export const meta: MetaFunction = () => {
   return [
@@ -8,34 +11,11 @@ export const meta: MetaFunction = () => {
 };
 
 export default function Index() {
+  const { session } = useOutletContext<OutletContext>();
+
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+    <div className="container mx-auto h-screen md:w-[800px]">
+      {!session?.user && <Login />}
     </div>
   );
 }
